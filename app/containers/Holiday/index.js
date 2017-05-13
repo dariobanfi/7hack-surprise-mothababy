31275b669bfe0e665ff104abf590c242c20316b3
Dario Banfi <dario.banfi.extern@holidaycheck.com>
2017-05-13 16:05:08 +0200

    Spying to see what item is in viewport 🚽

--- app/containers/Holiday/index.js
+++ app/containers/Holiday/index.js
@@ -10,28 +10,42 @@ import { createStructuredSelector } from 'reselect';
 
 import { makeSelectCurrentUser } from 'containers/App/selectors';
 import Img from "../../components/Header/Img";
+import Waypoint from 'react-waypoint';
 
 
 export class Holiday extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
+  printImages() {
+    const imagesWithMetadata = [
+      { img: 'https://www.placecage.com/g/400/500', tags: ['beach']},
+      { img: 'https://www.placecage.com/g/400/500', tags: ['beach']},
+      { img: 'https://www.placecage.com/g/400/500', tags: ['beach']},
+      { img: 'https://www.placecage.com/g/400/500', tags: ['beach']},
+      { img: 'https://www.placecage.com/g/400/500', tags: ['beach']},
+      { img: 'https://www.placecage.com/g/400/500', tags: ['beach']},
+      { img: 'https://www.placecage.com/g/400/500', tags: ['beach']}
+    ];
+
+    return imagesWithMetadata.map((metadataImg, index) => {
+      return (
+        <div key={index}>
+          <Img src={metadataImg.img} />
+          <Waypoint
+            onEnter={(evt) => console.log('Looking at', metadataImg.img)}
+          />
+      </div>
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <Img src="https://www.placecage.com/g/400/500"></Img>
-        <Img src="https://www.placecage.com/g/400/500"></Img>
-        <Img src="https://www.placecage.com/g/400/500"></Img>
-        <Img src="https://www.placecage.com/g/400/500"></Img>
-        <Img src="https://www.placecage.com/g/400/500"></Img>
-        <Img src="https://www.placecage.com/g/400/500"></Img>
-        <Img src="https://www.placecage.com/g/400/500"></Img>
+        {this.printImages()}
       </div>
     );
   }
 }
-
-Holiday.propTypes = {
-  item: React.PropTypes.object,
-  currentUser: React.PropTypes.string,
-};
 
 export default connect(createStructuredSelector({
   currentUser: makeSelectCurrentUser(),
