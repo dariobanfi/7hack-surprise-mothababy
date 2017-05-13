a029ad03544c37ff0fdcc5dc6b67a6dd4affcc20
Dario Banfi <dario.banfi.extern@holidaycheck.com>
2017-05-13 14:20:21 +0200

    Simple holiday

--- app/containers/Holiday/index.js
+++ app/containers/Holiday/index.js
@@ -1,37 +1,29 @@
-/*
+/**
+ * RepoListItem
  *
- * LanguageProvider
- *
- * this component connects the redux state language locale to the
- * IntlProvider component and i18n messages (loaded from `app/translations`)
+ * Lists the name and the issue count of a repository
  */
 
 import React from 'react';
 import { connect } from 'react-redux';
-import { createSelector } from 'reselect';
-import { IntlProvider } from 'react-intl';
+import { createStructuredSelector } from 'reselect';
 
-import { makeSelectLocale } from './selectors';
+import { makeSelectCurrentUser } from 'containers/App/selectors';
 
-export class LanguageProvider extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+
+export class Holiday extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <IntlProvider locale={this.props.locale} key={this.props.locale} messages={this.props.messages[this.props.locale]}>
-        {React.Children.only(this.props.children)}
-      </IntlProvider>
+      <div>Holidays go here</div>
     );
   }
 }
 
-LanguageProvider.propTypes = {
-  locale: React.PropTypes.string,
-  messages: React.PropTypes.object,
-  children: React.PropTypes.element.isRequired,
+Holiday.propTypes = {
+  item: React.PropTypes.object,
+  currentUser: React.PropTypes.string,
 };
 
-const mapStateToProps = createSelector(
-  makeSelectLocale(),
-  (locale) => ({ locale })
-);
-
-export default connect(mapStateToProps)(LanguageProvider);
+export default connect(createStructuredSelector({
+  currentUser: makeSelectCurrentUser(),
+}))(Holiday);
