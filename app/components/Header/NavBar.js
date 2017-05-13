1f7984763f32033072c7ef687ac9fc3e603844d8
nborrmann <n.borrmann@googlemail.com>
2017-05-13 22:10:35 +0200

    some styling

--- app/components/Header/NavBar.js
+++ app/components/Header/NavBar.js
@@ -2,8 +2,9 @@ import styled from 'styled-components';
 
 export default styled.div`
   text-align: center;
-  background-color: #fff;
+  background-color: orange;
   position: fixed;
   width: 100%;
   box-shadow: 0px 2px 20px #000;
+  font-weight: bold;
 `;
